7ba1157cd352be341826da9d7ad09d2a8a0994f2
dbartnickiCL <daniel.bartnicki@codilime.com>
2022-07-12 20:49:21 +0200

    add two tests

--- test_rename.py
+++ test_rename.py
@@ -2,3 +2,7 @@ import calculator
 def test_add():
     assert calculator.add(2,2) == 4
     assert calculator.add(2,5) == 7
+def test_add2():
+    assert calculator.add(3,9) == 12
+def test_add3():
+    assert calculator.add(1,2) == 3
