1dfbc76251ccd881979799c2b85c8ec44bbe33bc
dbartnickiCL <daniel.bartnicki@codilime.com>
2022-07-13 08:59:35 +0200

    add docker run line, renaname docker test

--- test_rename.py
+++
@@ -1,8 +0,0 @@
-import calculator
-def test_add():
-    assert calculator.add(2,2) == 4
-    assert calculator.add(2,5) == 7
-def test_add2():
-    assert calculator.add(3,9) == 12
-def test_add3():
-    assert calculator.add(1,2) == 0, "3 is not equal 0"
